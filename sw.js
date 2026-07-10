b0a56ced60f306045f91145256966e112d656e21
ktwork4831-commits <ktwork4831@gmail.com>
2026-07-10 23:55:50 +0900

    PWAキャッシュを更新

--- sw.js
+++ sw.js
@@ -1,24 +1,26 @@
-const CACHE_NAME = "simple-music-import-noaccept-v3";
+const CACHE_NAME = "simple-music-import-noaccept-v4";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "./manifest.webmanifest"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)))
   );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
 });
